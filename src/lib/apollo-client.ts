8e5625248c20dd4ebc9d23049499fd61ac8eb5eb
Ardione321 <ardioned@gmail.com>
2025-06-10 01:50:16 +0800

    feat: add README and update Apollo client to use env variable for GraphQL URI

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql', 
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI || 'http://localhost:3000/graphql', 
   cache: new InMemoryCache(),
 });
 
